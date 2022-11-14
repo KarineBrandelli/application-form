a0c4493c9fc23a15738ee6994beb2f58c32fcad0
Karine Brandelli <karinebrandelli@live.com>
2022-11-14 14:23:03 -0300

    age validation complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const camposDoFormulario = document.querySelectorAll("[required]");
 const formulario = document.querySelector('.form');
+console.log(camposDoFormulario)
 
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,21 +14,22 @@ function verificaCampo(campo){
   if (campo.name == "aniversario" && campo.value != "") {
     maiorDeIdade(campo);
   }
-}
+};
 
 function maiorDeIdade(campo) {
   const dataNascimento = new Date(campo.value);
+  const erroAniversario = document.querySelector('.error-message-aniversario');
   if (!validaIdade(dataNascimento)){
-    campo.setCustomValidity('O usuário não é maior de idade.')
+    erroAniversario.innerHTML = 'O usuário não é maior de idade.';
   }
-}
+};
 
 function validaIdade(data) {
   const dataAtual = new Date();
   const dataMais18 = new Date(
-    data.getUFCFullYear() + 18,
+    data.getUTCFullYear() + 18,
     data.getUTCMonth(),
     data.getUTCDate());
 
   return dataAtual >= dataMais18
-}
+};
